2599801ce106a5706f2a537a30a503618d75a09f
RiSKY247 <eyosieyob7@gmail.com>
2026-08-08 14:00:59 +0300

    Create layout.tsx

---
+++ src/app/layout.tsx
@@ -0,0 +1,16 @@
+export const metadata = {
+  title: 'Cassiopeia Tours',
+  description: 'Tour booking system',
+}
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode
+}) {
+  return (
+    <html lang="en">
+      <body>{children}</body>
+    </html>
+  )
+}
